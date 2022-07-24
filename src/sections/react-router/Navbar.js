66f790162f630ec5de29d4f3e2052027f7914bbc
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-24 23:41:59 +0200

    Working on a router.

--- src/sections/react-router/Navbar.js
+++ src/sections/react-router/Navbar.js
@@ -1,7 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <nav>navbar</nav>;
+  return (
+    <nav>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/people">People</Link>
+          </li>
+        </ul>
+      </Router>
+    </nav>
+  );
 };
 
 export default Navbar;
